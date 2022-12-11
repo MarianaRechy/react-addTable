fff51957cd11cc85e5e05a5eedb241f78d9958eb
rechy <rechy.mr@gmail.com>
2022-12-11 16:12:32 -0600

    addRandomNumber

--- src/components/TodoIntem/TodoItem.js
+++ src/components/TodoIntem/TodoItem.js
@@ -3,10 +3,14 @@ import { Box } from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 
-const TodoItem = ({ text, completed, completeTodo, index }) => {
+const TodoItem = ({ text, completeTodo, index }) => {
   const icontrue = <CheckIcon />;
   const iconfalse = <CloseIcon />;
   let newtask = !{ completeTodo };
+
+  let maxNumber = 100;
+  let randomId = Math.floor(Math.random() * maxNumber + 1);
+  let randomUser = Math.floor(Math.random() * maxNumber + 2);
 
   return completeTodo ? (
     <Box
@@ -22,7 +26,7 @@ const TodoItem = ({ text, completed, completeTodo, index }) => {
         alignItems="center"
         sx={{ pr: 3, border: 1, width: 20 }}
       >
-         id
+        {randomId}
       </Box>
       <Box
         display="flex"
@@ -31,7 +35,7 @@ const TodoItem = ({ text, completed, completeTodo, index }) => {
         alignItems="center"
         sx={{ pr: 3, border: 1, width: 20 }}
       >
-        User
+        {randomUser}
       </Box>
       <Box sx={{ pr: 3, border: 1, width: 300 }}>{text}</Box>
       <Box
@@ -47,13 +51,3 @@ const TodoItem = ({ text, completed, completeTodo, index }) => {
 };
 
 export default TodoItem;
-
-/*
-  const [task, setTask] = useState( !{completeTodo} );
-  
-  const defaultTask = (() =>{
-    setTask (completeTodo)
-  })
-
-  const newtask= defaultTask(task)
-*/
